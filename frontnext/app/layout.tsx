e53e96c22626c6a63ac860bb5a49ee47ed1a4bad
pllara <freelance.pedrolara@gmail.com>
2024-01-31 17:30:53 -0300

    .

--- frontnext/app/layout.tsx
+++ frontnext/app/layout.tsx
@@ -7,8 +7,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Meta Tracker",
+  description: "Defina metas e acompanhe seu progresso",
 };
 
 export default function RootLayout({
--- frontnext/app/page.tsx
+++ frontnext/app/page.tsx
@@ -71,7 +71,7 @@ const CadastroScreen = () => {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Login - Meta Tracker</title>
-        <link rel="shortcut icon" type="image/x-icon" href="Login/Imagens/CheckIcon.png" />
+        <link rel="shortcut icon" type="image/x-icon" href={'/favicon.png'} />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet" />
